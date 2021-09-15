8c9ff0bcb71cf39ab39d623a675379f1de6904fb
David Karn <david@webdever.net>
2021-09-15 13:59:19 -0700

    search contacts view
    Former-commit-id: 34535e6d06e222b5183c3ef335380e02274d30c3

--- lib/src/contacts/recent_contacts.dart
+++ lib/src/contacts/recent_contacts.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:fusion_mobile_revamped/src/components/contact_circle.dart';
+import 'package:fusion_mobile_revamped/src/components/fusion_dropdown.dart';
 import 'package:fusion_mobile_revamped/src/models/call_history.dart';
 import 'package:fusion_mobile_revamped/src/models/contact.dart';
 import 'package:fusion_mobile_revamped/src/models/conversations.dart';
@@ -25,6 +26,7 @@ class _RecentContactsTabState extends State<RecentContactsTab> {
   SMSConversation openConversation = null;
   bool _showingResults = false;
   String _selectedTab = 'all';
+  String _query = '';
 
   _getTitle() {
     return {
@@ -90,15 +92,162 @@ class _RecentContactsTabState extends State<RecentContactsTab> {
     List<Widget> children;
     //if (openConversation == null) {
     children = [
-      SearchContactsBar(_fusionConnection, (String query) {}, () {
+      SearchContactsBar(_fusionConnection, (String query) {
+        this.setState(() {
+          if (query.trim() != "") {
+            print("searching now " + query);
+            _showingResults = true;
+            _query = query;
+          }
+          else {
+            _showingResults = false;
+          }
+        });
+      }, () {
         this.setState(() {
           _showingResults = false;
         });
       }),
-      _tabBar(),
-      ContactsList(_fusionConnection, _getTitle(), _selectedTab)
+      !_showingResults ? _tabBar() : Container(),
+      (_showingResults
+          ? ContactsSearchList(_fusionConnection, _query)
+          : ContactsList(_fusionConnection, _getTitle(), _selectedTab))
     ];
     return Container(child: Column(children: children));
+  }
+}
+
+class ContactsSearchList extends StatefulWidget {
+  final FusionConnection _fusionConnection;
+  final String _query;
+
+  ContactsSearchList(this._fusionConnection, this._query, {Key key})
+      : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _ContactsSearchListState();
+}
+
+class _ContactsSearchListState extends State<ContactsSearchList> {
+  FusionConnection get _fusionConnection => widget._fusionConnection;
+
+  String get _query => widget._query;
+  int lookupState = 0; // 0 - not looking up; 1 - looking up; 2 - got results
+  List<Contact> _contacts = [];
+  String _lookedUpQuery;
+  String _selectedTab;
+  String _typeFilter = "All Contacts";
+
+  initState() {
+    super.initState();
+  }
+
+  _lookupQuery() {
+    print("contactssearch looking up " + _query);
+    lookupState = 1;
+    _lookedUpQuery = _query;
+    _fusionConnection.contacts.search(_query, 100, 0, (List<Contact> contacts) {
+      print("gotresult" + contacts.toString());
+      this.setState(() {
+        lookupState = 2;
+        _contacts = contacts;
+      });
+    });
+  }
+
+  _resultRow(String letter, Contact contact) {
+    return Row(
+      children: [
+        Container(
+            width: 32,
+            height: 50,
+            child: Align(
+                alignment: Alignment.centerLeft,
+                child: letter.length > 0
+                    ? Text(letter.toUpperCase(),
+                        style: TextStyle(
+                            color: smoke,
+                            fontSize: 16,
+                            height: 1,
+                            fontWeight: FontWeight.w500))
+                    : Container())),
+        ContactCircle.withDiameter([contact], [], 36),
+        Expanded(
+            child: Column(children: [
+          Align(
+              alignment: Alignment.centerLeft,
+              child: Text(contact.name,
+                  style: TextStyle(
+                      color:
+                      coal, fontSize: 14, fontWeight: FontWeight.w700)))
+        ]))
+      ],
+    );
+  }
+
+  _searchList() {
+    _contacts.sort((a, b) {
+      return (a.lastName + a.firstName)
+          .trim()
+          .toLowerCase()
+          .compareTo((b.lastName + b.firstName).trim().toLowerCase());
+    });
+    String usingLetter = '';
+    List<Widget> rows = [];
+    _contacts.forEach((item) {
+      String letter = (item.lastName + item.firstName).trim()[0].toLowerCase();
+      if (usingLetter != letter) {
+        usingLetter = letter;
+      } else {
+        letter = "";
+      }
+      rows.add(_resultRow(letter, item));
+    });
+    print("rows" + rows.toString());
+    return rows;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_lookedUpQuery != _query) {
+      lookupState = 0;
+    }
+    if (lookupState == 0) {
+      _lookupQuery();
+    }
+
+    return Expanded(
+        child: Container(
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(16))),
+            padding: EdgeInsets.only(top: 0, left: 0, right: 0, bottom: 0),
+            child: Column(children: [
+              Container(
+                  padding: EdgeInsets.only(left: 12),
+                  child: FusionDropdown(
+                      onChange: (String value) {
+                        this.setState(() {
+                          _typeFilter = value;
+                        });
+                      },
+                      value: _typeFilter,
+                      label: "Contact Type",
+                      options: [
+                        ["ALL CONTACTS", "All Contacts"],
+                        ["INTEGRATED CONTACTS", "Integrated Contacts"],
+                        ["COWORKERS", "Coworkers"],
+                        ["FUSION CONTACTS", "Fusion Contacts"]
+                      ],
+                      style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w700,
+                          color: coal))),
+              Expanded(
+                  child: ListView(
+                      padding: EdgeInsets.only(left: 12, right: 12),
+                      children: _searchList())),
+            ])));
   }
 }
 
@@ -260,23 +409,22 @@ class _CallHistorySummaryViewState extends State<CallHistorySummaryView> {
     return Expanded(
         child: GestureDetector(
             onTap: onTap,
-                child: Opacity(
-        opacity: 0.66,
-        child: Container(
-
-            margin: EdgeInsets.only(left:12),
+            child: Opacity(
+                opacity: 0.66,
+                child: Container(
+                    margin: EdgeInsets.only(left: 12),
                     child: Row(children: [
                       Container(
-
-                          width: width, height: height,
+                          width: width,
+                          height: height,
                           child: Image.asset("assets/icons/" + icon + ".png",
-                                   width: width, height: height)),
+                              width: width, height: height)),
                       Text(" " + label,
-                               style: TextStyle(
-                                   color: coal,
-                                   fontSize: 14,
-                                   fontWeight: FontWeight.w800))
-                ])))));
+                          style: TextStyle(
+                              color: coal,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w800))
+                    ])))));
   }
 
   @override
@@ -284,21 +432,22 @@ class _CallHistorySummaryViewState extends State<CallHistorySummaryView> {
     List<Widget> children = [_topPart()];
 
     if (_expanded) {
-      children.add(Container(child: horizontalLine(0),
-                             margin: EdgeInsets.only(top:4, bottom:4)));
       children.add(Container(
-          height:28,
+          child: horizontalLine(0),
+          margin: EdgeInsets.only(top: 4, bottom: 4)));
+      children.add(Container(
+          height: 28,
           margin: EdgeInsets.only(top: 12, bottom: 12),
           child: Row(children: [
             _actionButton("Profile", "user_dark", 18, 18, () {}),
             _actionButton("Call", "phone_dark", 18, 18, () {}),
             _actionButton("Message", "message_dark", 18, 18, () {}),
-           // _actionButton("Video", "video_dark", 18, 18, () {}),
-      ])));
+            // _actionButton("Video", "video_dark", 18, 18, () {}),
+          ])));
     }
 
     return Container(
-        height: _expanded ? 132: 76,
+        height: _expanded ? 132 : 76,
         padding: EdgeInsets.all(4),
         margin: EdgeInsets.only(bottom: 12),
         decoration: _expanded
@@ -315,50 +464,54 @@ class _CallHistorySummaryViewState extends State<CallHistorySummaryView> {
   }
 
   _topPart() {
-    return Row(children: [
-      ContactCircle(_contacts(), _crmContacts()),
-      Expanded(
-          child: GestureDetector(
-              onTap: () {
-                _expand();
-              },
-              child: Column(children: [
-                Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(_name(),
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 16))),
-                Align(
-                    alignment: Alignment.centerLeft,
-                    child: Row(children: [
-                      Container(
-                          margin: EdgeInsets.only(top: 4),
-                          decoration: BoxDecoration(
-                            color: _expanded
-                                ? Colors.white
-                                : Color.fromARGB(255, 243, 242, 242),
-                            borderRadius: BorderRadius.all(Radius.circular(4)),
-                          ),
-                          padding: EdgeInsets.only(
-                              left: 6, right: 6, top: 2, bottom: 2),
-                          child: Row(children: [
-                            Image.asset(_icon(), width: 12, height: 12),
-                            Text(
-                                " " +
-                                    mDash +
+    return GestureDetector(
+        onTap: () {
+          _expand();
+        },
+        child: Row(children: [
+          ContactCircle(_contacts(), _crmContacts()),
+          Expanded(
+              child: Container(
+                  decoration: BoxDecoration(color: Colors.transparent),
+                  child: Column(children: [
+                    Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(_name(),
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 16))),
+                    Align(
+                        alignment: Alignment.centerLeft,
+                        child: Row(children: [
+                          Container(
+                              margin: EdgeInsets.only(top: 4),
+                              decoration: BoxDecoration(
+                                color: _expanded
+                                    ? Colors.white
+                                    : Color.fromARGB(255, 243, 242, 242),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(4)),
+                              ),
+                              padding: EdgeInsets.only(
+                                  left: 6, right: 6, top: 2, bottom: 2),
+                              child: Row(children: [
+                                Image.asset(_icon(), width: 12, height: 12),
+                                Text(
                                     " " +
-                                    DateFormat.jm()
-                                        .format(_historyItem.startTime),
-                                style: TextStyle(
-                                    color: _isMissed() ? crimsonLight : coal,
-                                    fontSize: 12,
-                                    height: 1.4,
-                                    fontWeight: FontWeight.w400))
-                          ])),
-                      Expanded(child: Container())
-                    ]))
-              ])))
-    ]);
+                                        mDash +
+                                        " " +
+                                        DateFormat.jm()
+                                            .format(_historyItem.startTime),
+                                    style: TextStyle(
+                                        color:
+                                            _isMissed() ? crimsonLight : coal,
+                                        fontSize: 12,
+                                        height: 1.4,
+                                        fontWeight: FontWeight.w400))
+                              ])),
+                          Expanded(child: Container())
+                        ]))
+                  ])))
+        ]));
   }
 }
 
