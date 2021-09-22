811edffc501508c04455259988ef0d141df8237c
Chris Windsor <chris@simplii.net>
2021-09-22 15:48:42 -0600

    Improved on-hold view for callpop
    Former-commit-id: 7deab7239ea07f8453445d052ceef3abff87dce8

---
+++ lib/src/callpop/call_notes.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CallNotes extends StatefulWidget {
+  CallNotes({Key key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _CallNotesState();
+}
+
+class _CallNotesState extends State<CallNotes> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
