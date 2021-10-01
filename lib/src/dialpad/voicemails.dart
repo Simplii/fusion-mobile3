f9811d4f3e26e73a82d819ba2093068d0e3d1120
Chris Windsor <chris@simplii.net>
2021-10-01 09:18:02 -0600

    Implemented tab controller for dialpad view
    Former-commit-id: 696d249c686a5f8d47c892ce3772db523dabcd12

--- lib/src/dialpad/voicemails.dart
+++ lib/src/dialpad/voicemails.dart
@@ -2,18 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:fusion_mobile_revamped/src/backend/fusion_connection.dart';
 import 'package:fusion_mobile_revamped/src/backend/softphone.dart';
 
-class ContactsSearch extends StatefulWidget {
-  ContactsSearch(this._fusionConnection, this._softphone, {Key key})
+class Voicemails extends StatefulWidget {
+  Voicemails(this._fusionConnection, this._softphone, {Key key})
       : super(key: key);
 
   final FusionConnection _fusionConnection;
   final Softphone _softphone;
 
   @override
-  State<StatefulWidget> createState() => _ContactsSearchState();
+  State<StatefulWidget> createState() => _VoicemailsState();
 }
 
-class _ContactsSearchState extends State<ContactsSearch> {
+class _VoicemailsState extends State<Voicemails> {
   FusionConnection get _fusionConnection => widget._fusionConnection;
 
   Softphone get _softphone => widget._softphone;
@@ -23,7 +23,7 @@ class _ContactsSearchState extends State<ContactsSearch> {
     return Container(
       child: Column(
         children: [
-
+          Text('Voicemails')
         ],
       ),
     );
