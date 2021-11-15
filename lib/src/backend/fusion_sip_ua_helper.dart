36041785c1ed92a4d10997e42abb1b833d456281
David Karn <david@webdever.net>
2021-11-15 06:20:00 -0600

    hold fix, use fusion wss push proxy server
    Former-commit-id: d87b1d858ccb31e48cda64865ac6d33837b3875b

---
+++ lib/src/backend/fusion_sip_ua_helper.dart
@@ -0,0 +1,24 @@
+import 'package:sip_ua/sip_ua.dart';
+
+class FusionSIPUAHelper extends SIPUAHelper {
+  bool _settingVideo = false;
+
+  setVideo(bool settingVideo) {
+    print("settingvideo:"+_settingVideo.toString());
+    _settingVideo = settingVideo;
+  }
+
+  @override
+  Map<String, Object> buildCallOptions([bool voiceonly = false]) {
+    var options = super.buildCallOptions(voiceonly);
+    // if this isn't set for a hold request, reinvites will not be sent out for holds on outbound calls
+    // if it is seton makecall, the app crashes
+    if (_settingVideo == true) {
+      ((options['rtcOfferConstraints'] as Map<String, Object>)['mandatory']
+      as Map<String, dynamic>)['OfferToReceiveVideo'] = true;
+    }
+    print("builtoptions");
+    print(options);
+    return options;
+  }
+}
