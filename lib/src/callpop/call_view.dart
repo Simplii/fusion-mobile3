6fa2fde8c40237c57b11fd622490920f4552e311
Zaid Jamil <zaid@simplii.net>
2022-12-16 16:21:48 -0700

    IOS bt changes
    Former-commit-id: b646807bd2ab24a79764c1a24f45f1e136427705

--- lib/src/callpop/call_view.dart
+++ lib/src/callpop/call_view.dart
@@ -269,7 +269,7 @@ class _CallViewState extends State<CallView> {
       ["assets/icons/call_view/audio_speaker.png", "Speaker", "speaker"],
       ["assets/icons/call_view/bluetooth.png", "Bluetooth", "bluetooth"],
     ];
-
+    print(["call_view", _softphone.activeCallOutput, _softphone.outputDevice]);
     String callAudioOutput = _softphone.activeCallOutput != ''
         ? _softphone.activeCallOutput
         : _softphone.outputDevice;
@@ -277,7 +277,7 @@ class _CallViewState extends State<CallView> {
         ? _softphone.activeCallOutputDevice
         : _softphone.defaultOutput;
     bool muted = _softphone.getMuted(_activeCall);
-    
+
     showModalBottomSheet(
         context: context,
         backgroundColor: Colors.transparent,
