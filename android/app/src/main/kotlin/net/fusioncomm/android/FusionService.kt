a4011fed34dc11b026bebe2d586cda14094ec412
Zaid Jamil <zaid@simplii.net>
2022-12-28 11:39:13 -0700

    fixed keep new calls on current output & added service running check & kill all calls when app terminate
    Former-commit-id: 168d611cb711a011b890f010b72638a6d34a6cd4

--- android/app/src/main/kotlin/net/fusioncomm/android/FusionService.kt
+++ android/app/src/main/kotlin/net/fusioncomm/android/FusionService.kt
@@ -4,25 +4,38 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
+import io.flutter.plugin.common.MethodChannel
 import org.linphone.core.tools.service.CoreManager
 
 
 class FusionService: Service() {
 
+    companion object {
+       var serviceStarted: Boolean = false
+    }
+
     override fun onBind(intent: Intent?): IBinder? {
         return null
     }
 
+    override fun onCreate() {
+        serviceStarted = true
+        super.onCreate()
+    }
     override fun onTaskRemoved(rootIntent: Intent?) {
         Log.d("fusionService","onTaskRemoved called")
         val core = CoreManager.instance().core
-        core.currentCall?.terminate()
+        for ( call in core.calls){
+            call.terminate()
+        }
         super.onTaskRemoved(rootIntent)
         //stop service
         this.stopSelf()
+        serviceStarted = false
     }
 
     override fun onDestroy() {
+        serviceStarted = false
         super.onDestroy()
         Log.d("fusionService","destroyed")
     }
