e9c62cdae34db3f47e244f835bd57b4eb30edf48
Zaid Jamil <zaid@simplii.net>
2023-08-09 13:59:20 -0600

    clean up
    Former-commit-id: 64e5e7a5f9688ca26a7bac4d2ade4489fb712f7b

--- lib/src/backend/fusion_connection.dart
+++ lib/src/backend/fusion_connection.dart
@@ -81,9 +81,9 @@ class FusionConnection {
   Connectivity connectivity = Connectivity();
   ConnectivityResult connectivityResult = ConnectivityResult.none;
   bool internetAvailable = true;
-  String serverRoot = "http://fusion-test.fusioncomm.net";
+  String serverRoot = "http://fusioncomm.net";
   String mediaServer = "https://fusion-media.sfo2.digitaloceanspaces.com";
-  String defaultAvatar = "https://fusion-test.fusioncomm.net/img/defaultuser.png";
+  String defaultAvatar = "https://fusioncomm.net/img/defaultuser.png";
 
   FusionConnection() {
     _getCookies();
@@ -140,7 +140,7 @@ class FusionConnection {
   }
 
   final channel = WebSocketChannel.connect(
-    Uri.parse('wss://fusion-test.fusioncomm.net:8443'),
+    Uri.parse('wss://fusioncomm.net:8443'),
   );
 
   onLogOut(Function callback) {
@@ -304,7 +304,7 @@ class FusionConnection {
       data['username'] = await _getUsername();
 
       Uri url = Uri.parse(
-          'https://fusion-test.fusioncomm.net/api/v1/clients/api_request?username=' +
+          'https://fusioncomm.net/api/v1/clients/api_request?username=' +
               data['username']);
       Map<String, String> headers = await _cookieHeaders(url);
       String body = convert.jsonEncode(data);
@@ -349,9 +349,8 @@ class FusionConnection {
           urlParams += key + "=" + Uri.encodeQueryComponent(data[key].toString().trim().replaceAll(reg, '')) + '&';
         }
       }
-      Uri url = Uri.parse('https://fusion-test.fusioncomm.net/api/v1' + route + urlParams);
+      Uri url = Uri.parse('https://fusioncomm.net/api/v1' + route + urlParams);
       Map<String, String> headers = await _cookieHeaders(url);
-      print(convert.jsonEncode(data));
       if (method.toLowerCase() != 'get') {
         args[#body] = convert.jsonEncode(data);
         headers["Content-Type"] = "application/json";
@@ -424,7 +423,7 @@ class FusionConnection {
           urlParams += key + "=" + Uri.encodeQueryComponent(data[key].toString()) + '&';
         }
       }
-      Uri url = Uri.parse('https://fusion-test.fusioncomm.net/api/v2' + route + urlParams);
+      Uri url = Uri.parse('https://fusioncomm.net/api/v2' + route + urlParams);
       Map<String, String> headers = await _cookieHeaders(url);
 
       if (method.toLowerCase() != 'get') {
@@ -481,7 +480,7 @@ class FusionConnection {
     try {
       data['username'] = await _getUsername();
 
-      Uri url = Uri.parse('https://fusion-test.fusioncomm.net/api/v2' + route);
+      Uri url = Uri.parse('https://fusioncomm.net/api/v2' + route);
       http.MultipartRequest request = new http.MultipartRequest(method, url);
       (await _cookieHeaders(url))
           .forEach(
@@ -660,7 +659,7 @@ print(responseBody);
 
   setupSocket() {
     int messageNum = 0;
-    final wsUrl = Uri.parse('wss://fusion-test.fusioncomm.net:8443/');
+    final wsUrl = Uri.parse('wss://fusioncomm.net:8443/');
     socketChannel = WebSocketChannel.connect(wsUrl);
     socketChannel.stream.listen((messageData) {
       Map<String, dynamic> message = convert.jsonDecode(messageData);
